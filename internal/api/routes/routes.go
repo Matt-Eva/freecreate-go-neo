37d0239583e7351d1c5d0b3ff39fe5a8d33d87d0
Matt Eva <matteweva@gmail.com>
2024-05-26 15:19:43 -0700

    set up basic api file structure

---
+++ internal/api/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"fmt"
+	// "net/http"
+)
+
+func CreateRoutes() error {
+	fmt.Println("runngin")
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
-	
+import (
+	"freecreate/internal/api/routes"
+	"fmt"
+	"os"
+)
+
+func run() error {
+	if err := routes.CreateRoutes(); err !=nil{
+		return err
+	}
+	return nil
+}
+
+func main() {
+
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
