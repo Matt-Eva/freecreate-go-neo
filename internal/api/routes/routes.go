e344ff14b9243f1c9a94bb69f465cbd1ec1f422e
Matt Eva <matteweva@gmail.com>
2024-05-30 17:30:35 -0700

    set up mock search endpoint - successfully receiving query params

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -1,17 +1,24 @@
 package routes
 
 import (
-	// "encoding/json"
-	"net/http"
-	"github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
 	"freecreate/internal/api/handlers"
 	"freecreate/internal/api/middleware"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateRoutes(neo, mongo, redis string) error {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api", middleware.AddDrivers(handlers.TestHandler, neo, mongo, redis))
+	router.HandleFunc("/api/search", func(w http.ResponseWriter, r *http.Request){
+		params := r.URL.Query()
+		fmt.Println(params)
+		json.NewEncoder(w).Encode(params)
+	})
 
 	return http.ListenAndServe(":8080", router)
 }
