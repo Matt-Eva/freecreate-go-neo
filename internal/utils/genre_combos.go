9d3f1032bd0dc20551662cf26658bcf0df81713a
Matt Eva <matteweva@gmail.com>
2024-06-21 14:44:05 -0700

    added preliminary code to generate genre combos

--- internal/utils/genre_combos.go
+++ internal/utils/genre_combos.go
@@ -4,6 +4,40 @@ import (
 	"fmt"
 )
 
-func GenerateGenreCombos () {
+func GenerateGenreCombos () [][]string {
 	genres := GetGenres()
+	genreMap := make(map[string]bool)
+	genreCombos := make([][]string)
+
+	for _, genre := range genres{
+		genreCombos = append(genreCombos, []string{genre})
+	}
+
+	for _, genre := genres {
+		for _, g := genres {
+			sorted := []string{genre, g}
+			slices.Sort(sorted)
+			combo := fmt.Sprintf("%s:%s", sorted[0], sorted[1])
+			if g != genre && !genreMap[combo] {
+				genreCombos = append(genreCombos, sorted)
+				genreMap[combo] = true
+			}
+		}
+	}
+
+	for _, genre := range genres{
+		for _, gen := range genres{
+			for _, g := range genres{
+				sorted := []string{genre, gen, g}
+				slices.Sort(sorted)
+				combo := fmt.Sprintf("%s:%s:%s", sorted[0],sorted[1], sorted[2])
+				if genre != gen && genre != g && gen != g && !genreMap[combo]{
+					genreCombos := append(genreCombos, sorted)
+					genreMap[combo] = true
+				} 
+			}
+		}
+	}
+
+	return genreCombos
 }
