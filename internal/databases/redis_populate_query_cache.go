f19672f67f65ad0eff13d098529f83c9b19f862f
Matt Eva <matteweva@gmail.com>
2024-06-22 22:05:13 -0700

    creating util and database functionality for simple search

---
+++ internal/databases/redis_populate_query_cache.go
@@ -0,0 +1,14 @@
+package databases
+
+// import (
+// 	"freecreate/internal/utils"
+// )
+
+// func PopulateRedisQueryCache() {
+// 	queryComboMap := utils.AssembleCachePopulationCombos()
+
+// 	for key, genreSlice := range queryComboMap {
+// 		for _, genreCombo := range genreSlice {
+// 		}
+// 	}
+// }
---
+++ internal/utils/validate_genres.go
@@ -0,0 +1,17 @@
+package utils
+
+func ValidateGenres(genreLabels []string) []string{
+	genres := GetGenres()
+	validatedLabels := make([]string, 0, 3)
+
+	for _, label := range genreLabels {
+		for _, genre := range genres {
+			if label == genre {
+				validatedLabels = append(validatedLabels, genre)
+				break
+			}
+		}
+	}
+	
+	return validatedLabels
+}
