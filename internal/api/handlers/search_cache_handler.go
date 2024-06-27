71e7e35c6eaec7c661b07e082088f97b8a1332fe
Matt Eva <matteweva@gmail.com>
2024-06-27 09:18:09 -0700

    created rest endpoint for cache search

---
+++ internal/api/handlers/search_cache_handler.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "net/http"
+
+
+func SearchCacheHandler(w http.ResponseWriter, r *http.Request, redis string){
+	
+}
--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -12,7 +12,8 @@ func CreateRoutes(neo, mongo, redis string) error {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api", middleware.AddDrivers(handlers.TestHandler, neo, mongo, redis)).Methods("GET")
-	router.HandleFunc("/api/search/most-recent", middleware.AddRedisDriver(handlers.SearchMostRecentHandler, redis)).Methods("GET")
+	router.HandleFunc("/api/search/most-recent", middleware.AddNeoDriver(handlers.SearchMostRecentHandler, neo)).Methods("GET")
+	router.HandleFunc("/api/search/cache", middleware.AddRedisDriver(handlers.SearchCacheHandler, redis)).Methods("GET")
 
 	return http.ListenAndServe(":8080", router)
 }
