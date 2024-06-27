b31442576ec2dbbf03815041afb8824991d00abd
Matt Eva <matteweva@gmail.com>
2024-06-27 09:15:52 -0700

    created rest endpoint for most recent search

---
+++ internal/api/handlers/search_most_recent_handler.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "net/http"
+
+func SearchMostRecentHandler(w http.ResponseWriter, r *http.Request, redis string){
+
+}
--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -12,7 +12,7 @@ func CreateRoutes(neo, mongo, redis string) error {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api", middleware.AddDrivers(handlers.TestHandler, neo, mongo, redis)).Methods("GET")
-	router.HandleFunc("/api/search", middleware.AddDrivers(handlers.SearchHandler, neo, mongo, redis)).Methods("GET")
+	router.HandleFunc("/api/search/most-recent", middleware.AddRedisDriver(handlers.SearchMostRecentHandler, redis)).Methods("GET")
 
 	return http.ListenAndServe(":8080", router)
 }
