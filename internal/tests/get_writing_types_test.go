7eb217bb9c9adcdf072c44e9bd8cfeb28660f388
Matt Eva <matteweva@gmail.com>
2024-06-26 17:58:33 -0700

    added test coverged to get writing types

---
+++ internal/tests/get_writing_types_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"freecreate/internal/utils"
+	"testing"
+)
+
+func TestGetWritingTypes(t *testing.T){
+	caseWritingTypes := map[string]bool{"shortStory": true, "novelette": true, "novella": true, "novel": true}
+	writingTypes := utils.GetWritingTypes()
+
+	for _, writingType := range writingTypes {
+		if !caseWritingTypes[writingType]{
+			t.Errorf("%s writing type not present in test case", writingType)
+		}
+		caseWritingTypes[writingType] = false
+	}
+
+	for key, val := range caseWritingTypes {
+		if val{
+			t.Errorf("%s writing type was not present in generated types", key)
+		}
+	}
+}
