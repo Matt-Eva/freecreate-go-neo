0889a60fb34c579761781eaea3fd699af4e17289
Matt Eva <matteweva@gmail.com>
2024-06-26 17:45:55 -0700

    added error handling to validate genres

--- internal/utils/validate_genres.go
+++ internal/utils/validate_genres.go
@@ -1,17 +1,32 @@
 package utils
 
-func ValidateGenres(genreLabels []string) []string {
+import (
+	"errors"
+	"fmt"
+)
+
+func ValidateGenres(genreLabels []string) ([]string, error) {
 	genres := GetGenres()
 	validatedLabels := make([]string, 0, 3)
+	validatedMap := make(map[string]bool)
 
 	for _, label := range genreLabels {
+		validatedMap[label] = false
 		for _, genre := range genres {
 			if label == genre {
 				validatedLabels = append(validatedLabels, genre)
+				validatedMap[label] = true
 				break
 			}
 		}
 	}
 
-	return validatedLabels
+	for key, present := range validatedMap{
+		if !present{
+			errorMsg := fmt.Sprintf("%s is not a valid genre", key)
+			return []string{}, errors.New(errorMsg)
+		}
+	}
+
+	return validatedLabels, nil
 }
