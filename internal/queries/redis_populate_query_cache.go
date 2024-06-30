fa4772e5669afaa5dcc614924c2c58a561a9a574
Matt Eva <matteweva@gmail.com>
2024-06-30 09:11:50 -0700

    removed uneccessary query files

--- internal/queries/redis_populate_query_cache.go
+++ internal/queries/redis_populate_query_cache.go
@@ -1,40 +1,35 @@
 package databases
 
-import (
-	"freecreate/internal/utils"
-	"strings"
-)
+// func PopulateRedisQueryCache() {
+// 	queryComboMap := utils.AssembleCachePopulationCombos()
+// 	neoQueries := make([]string, 0, 696*5*5)
 
-func PopulateRedisQueryCache() {
-	queryComboMap := utils.AssembleCachePopulationCombos()
-	neoQueries := make([]string, 0, 696*5*5)
+// 	for writingType, timeMap := range queryComboMap {
+// 		for timeFrame, genreSlices := range timeMap {
+// 			for _, genreSlice := range genreSlices {
+// 				neoQuery := BuildNeoPopulateCacheQuery(writingType, timeFrame, genreSlice)
+// 				neoQueries = append(neoQueries, neoQuery)
+// 			}
+// 		}
+// 	}
 
-	for writingType, timeMap := range queryComboMap {
-		for timeFrame, genreSlices := range timeMap {
-			for _, genreSlice := range genreSlices {
-				neoQuery := BuildNeoPopulateCacheQuery(writingType, timeFrame, genreSlice)
-				neoQueries = append(neoQueries, neoQuery)
-			}
-		}
-	}
+// }
 
-}
+// func BuildNeoPopulateCacheQuery(writingType, timeFrame string, genres []string) string {
+// 	validatedGenres := utils.ValidateGenres(genres)
+// 	validatedWritingType := utils.ValidateWritingType(writingType)
+// 	genreLabels := strings.Join(validatedGenres, ":")
+// 	timeFrameQuery := ""
 
-func BuildNeoPopulateCacheQuery(writingType, timeFrame string, genres []string) string {
-	validatedGenres := utils.ValidateGenres(genres)
-	validatedWritingType := utils.ValidateWritingType(writingType)
-	genreLabels := strings.Join(validatedGenres, ":")
-	timeFrameQuery := ""
+// 	if timeFrame == "Most Recent" {
 
-	if timeFrame == "Most Recent" {
+// 	} else if timeFrame == "All Time" {
 
-	} else if timeFrame == "All Time" {
+// 	}
 
-	}
+// 	return ""
+// }
 
-	return ""
-}
+// func BuildRedisPopulateCacheQuery() {
 
-func BuildRedisPopulateCacheQuery() {
-
-}
+// }
