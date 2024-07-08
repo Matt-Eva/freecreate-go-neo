85f5a4a1d3844e3b17c85d6306651594ef578f4a
Matt Eva <matteweva@gmail.com>
2024-07-07 18:10:30 -0700

    created cmd directory to enable running seeds

--- internal/seeds/user_seeds.go
+++ internal/seeds/user_seeds.go
@@ -1,5 +1,24 @@
 package seeds
 
+import (
+	"context"
+	"fmt"
+	"freecreate/internal/config"
+)
+
 func SeedUsers(){
-	// ctx := context.Background()
+	ctx := context.Background()
+	neo, err := config.InitNeo(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer neo.Close(ctx)
+
+	err = neo.VerifyConnectivity(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("neo connection successful")
 }
