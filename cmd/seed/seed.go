8eef2977619f56dca96543eab0128a588b67eaa3
Matt Eva <matteweva@gmail.com>
2024-07-07 18:16:34 -0700

    verified neo connectivity in seed file

--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -1,5 +1,34 @@
 package main
 
-func Seed(){
-	
+import (
+	"context"
+	"fmt"
+	"freecreate/internal/config"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main(){
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err.Error())
+		return 
+	}
+	fmt.Println(os.Getenv("NEO_USER"))
+	ctx := context.Background()
+	neo, err := config.InitNeo(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer neo.Close(ctx)
+
+	err = neo.VerifyConnectivity(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("neo connection successful")
+
 }
