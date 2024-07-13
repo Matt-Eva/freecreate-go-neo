0d13277d6ec0291f215042ecd35d23e71be54334
Matt Eva <matteweva@gmail.com>
2024-07-13 13:49:29 -0700

    created initial model files

--- internal/models/writing.go
+++ internal/models/writing.go
@@ -1,11 +1,22 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+
+
 type Writing struct {
 	Uid          string
 	Title        string
-	Descpription string
+	Description  string
 	Thumbnail    string
 	WritingType  string
+	CreatorId    string
 	CreatedAt    int64
 	UpdatedAt    int64
 	Libraries    int64
@@ -16,8 +27,85 @@ type Writing struct {
 	RelRank      int64
 }
 
+func (w Writing) validateNewWriting() error{
+	if w.Uid == ""{
+		e := fmt.Sprintf("uid cannot be empty")
+		return errors.New(e)
+	}
+	if w.Title == ""{
+		e := fmt.Sprintf("title cannot be empty")
+		return errors.New(e)
+	}
+	if w.Description == ""{
+		e := fmt.Sprintf("description cannot be empty")
+		return errors.New(e)
+	}
+
+	return nil
+}
+
+func (w Writing) newWritingParams() map[string]any{
+	writingParams := map[string]any {
+		"uid": w.Uid,
+	}
+
+	paramMap := map[string]any {
+		"writingParams": writingParams,
+		"creatorId": w.CreatorId,
+	}
+
+	return paramMap
+}
+
+type PostedWriting struct {
+	Title string
+	Description string
+	WritingType string
+	Thumbnail string
+	CreatorId string
+}
+
+func (p PostedWriting) generateWriting() Writing {
+	now := time.Now().UnixMilli()
+	newWriting := Writing {
+		Uid: uuid.New().String(),
+		Title: p.Title,
+		Description: p.Description,
+		Thumbnail: "",
+		WritingType: p.WritingType,
+		CreatorId: p.CreatorId,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	return newWriting
+}
+
+type UpdateWritingInfo struct {
+	Uid [16]byte
+	Title string
+	Description string
+	Thumbnail string
+	WritingType string
+}
+
+
+
 type ShortStory struct {
 	Writing
+}
+
+func (s ShortStory) validateNewShortStory() error {
+	err := s.validateNewWriting()
+	if err != nil {
+		return err
+	}
+
+	if s.WritingType != "shortStory"{
+		errorMsg := fmt.Sprintf("Writing type '%s' is not valid for a short Story; must be of type shortStory", s.WritingType)
+		return errors.New(errorMsg)
+	}
+	return nil
 }
 
 type Novel struct {
