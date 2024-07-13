17e364a7750050f10e2ad69e76e18cefe7ee7bb4
Matt Eva <matteweva@gmail.com>
2024-07-13 15:25:18 -0700

    generated base writing model, validator, and db param generator

--- internal/models/short_story.go
+++ internal/models/short_story.go
@@ -1,1 +1,23 @@
 package models
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ShortStory struct {
+	Writing
+}
+
+func (s ShortStory) validateNewShortStory(year int) error {
+	err := s.validateNewWriting(year)
+	if err != nil {
+		return err
+	}
+
+	if s.WritingType != "shortStory" {
+		errorMsg := fmt.Sprintf("Writing type '%s' is not valid for a short Story; must be of type shortStory", s.WritingType)
+		return errors.New(errorMsg)
+	}
+	return nil
+}
--- internal/models/writing.go
+++ internal/models/writing.go
@@ -2,13 +2,12 @@ package models
 
 import (
 	"errors"
-	"fmt"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 )
-
-
 
 type Writing struct {
 	Uid          string
@@ -19,95 +18,132 @@ type Writing struct {
 	CreatorId    string
 	CreatedAt    int64
 	UpdatedAt    int64
-	Libraries    int64
+	LibraryCount int64
 	Likes        int64
 	Views        int64
 	Donations    int64
 	Rank         int64
 	RelRank      int64
+	OriginalYear int
 }
 
-func (w Writing) validateNewWriting() error{
-	if w.Uid == ""{
-		e := fmt.Sprintf("uid cannot be empty")
+func (w Writing) validateNewWriting(year int) error {
+	if w.Uid == "" {
+		e := "uid cannot be empty"
 		return errors.New(e)
 	}
-	if w.Title == ""{
-		e := fmt.Sprintf("title cannot be empty")
+	if w.Title == "" {
+		e := "title cannot be empty"
 		return errors.New(e)
 	}
-	if w.Description == ""{
-		e := fmt.Sprintf("description cannot be empty")
+	if w.Description == "" {
+		e := "description cannot be empty"
+		return errors.New(e)
+	}
+	if w.Thumbnail != "" {
+		e := "thumbnail must be empty - not accepting thumbnail images at present"
+		return errors.New(e)
+	}
+	if w.WritingType == "" {
+		e := "writing type cannot be empty"
+		return errors.New(e)
+	}
+	if w.CreatorId == "" {
+		e := "creator id cannot be emtpy"
+		return errors.New(e)
+	}
+	if w.CreatedAt == 0 {
+		e := "server side error - no value inserted for created at"
+		return errors.New(e)
+	}
+	if w.UpdatedAt == 0 {
+		e := "server side error - no value inserted for updated at"
+		return errors.New(e)
+	}
+	if w.UpdatedAt != w.CreatedAt {
+		e := "server side error - created at and updated at must match"
+		return errors.New(e)
+	}
+	if w.LibraryCount != 0 {
+		e := "library count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.Likes != 0 {
+		e := "like count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.Donations != 0 {
+		e := "donation count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.Views != 0 {
+		e := "view count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.Rank != 0 {
+		e := "rank count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.RelRank != 0 {
+		e := "relrank count cannot be greater than 0 for new writing"
+		return errors.New(e)
+	}
+	if w.OriginalYear != year || w.OriginalYear == 0 {
+		e := "server side error - Original year does not match current year or is empty"
 		return errors.New(e)
 	}
 
 	return nil
 }
 
-func (w Writing) newWritingParams() map[string]any{
-	writingParams := map[string]any {
-		"uid": w.Uid,
+func (w Writing) newWritingParams() (map[string]any, error) {
+	v := reflect.ValueOf(w)
+	t := v.Type()
+	writingParams := make(map[string]any)
+
+	for i := 0; i < t.NumField(); i++ {
+		structField := t.Field(i).Name
+		mapField := strings.ToLower(structField[:1]) + structField[1:]
+		writingParams[mapField] = v.Field(i).Interface()
 	}
 
-	paramMap := map[string]any {
+	paramMap := map[string]any{
 		"writingParams": writingParams,
-		"creatorId": w.CreatorId,
+		"creatorId":     w.CreatorId,
 	}
 
-	return paramMap
+	return paramMap, nil
 }
 
 type PostedWriting struct {
-	Title string
-	Description string
-	WritingType string
-	Thumbnail string
-	CreatorId string
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	WritingType string `json:"writingType"`
+	Thumbnail   string `json:"thumbnail"`
+	CreatorId   string `json:"creatorId"`
 }
 
-func (p PostedWriting) generateWriting() Writing {
+func (p PostedWriting) generateWriting(year int) Writing {
 	now := time.Now().UnixMilli()
-	newWriting := Writing {
-		Uid: uuid.New().String(),
-		Title: p.Title,
-		Description: p.Description,
-		Thumbnail: "",
-		WritingType: p.WritingType,
-		CreatorId: p.CreatorId,
-		CreatedAt: now,
-		UpdatedAt: now,
+	newWriting := Writing{
+		Uid:          uuid.New().String(),
+		Title:        p.Title,
+		Description:  p.Description,
+		Thumbnail:    "",
+		WritingType:  p.WritingType,
+		CreatorId:    p.CreatorId,
+		CreatedAt:    now,
+		UpdatedAt:    now,
+		OriginalYear: year,
 	}
 
 	return newWriting
 }
 
 type UpdateWritingInfo struct {
-	Uid [16]byte
-	Title string
+	Uid         [16]byte
+	Title       string
 	Description string
-	Thumbnail string
+	Thumbnail   string
 	WritingType string
-}
-
-
-
-type ShortStory struct {
-	Writing
-}
-
-func (s ShortStory) validateNewShortStory() error {
-	err := s.validateNewWriting()
-	if err != nil {
-		return err
-	}
-
-	if s.WritingType != "shortStory"{
-		errorMsg := fmt.Sprintf("Writing type '%s' is not valid for a short Story; must be of type shortStory", s.WritingType)
-		return errors.New(errorMsg)
-	}
-	return nil
-}
-
-type Novel struct {
-	Writing
 }
