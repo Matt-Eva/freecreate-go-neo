4f584d4431c7b06f219803cffa882b7762163b76
Matt Eva <matteweva@gmail.com>
2024-07-15 17:32:08 -0700

    created genre model

--- internal/models/chapter.go
+++ internal/models/chapter.go
@@ -1,3 +1,10 @@
 package models
 
 // chapters exist only in Mongo, not in Neo4j
+
+type Chapter struct {
+	NeoId     string         `bson:"neo_id,omitempty"`
+	Content   map[string]any `bson:"content"`
+	CreatedAt int64          `bson:"created_at,omitempty"`
+	UpdatedAt int64 `bson:"updated_at,omitempty`
+}
---
+++ internal/models/genre.go
@@ -0,0 +1,6 @@
+package models
+
+type Genre struct {
+	Uid string
+	Genre string
+}
