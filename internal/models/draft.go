0c745fb78b51811e8a2b5aef028f66d65eee4689
Matt Eva <matteweva@gmail.com>
2024-07-21 12:23:12 -0700

    added comments to draft model

--- internal/models/draft.go
+++ internal/models/draft.go
@@ -2,12 +2,13 @@ package models
 
 // drafts exist only in Mongo, not in Neo4j
 
+// whenever a piece of content is edited, the draft itself is what is first updated
 type Draft struct {
-	NeoId       string         `bson:"neo_id"`
-	ContentId   string         `bson:"content_id"`
-	Content     map[string]any `bson:"content"`
-	Tags        []string       `bson:"tags"`
-	Genres      []string       `bson:"genres"`
+	NeoId       string         `bson:"neo_id"` // optional
+	ContentId   string         `bson:"content_id"` // optional
+	Content     map[string]any `bson:"content"` // optional
+	Tags        []string       `bson:"tags"` // optional
+	Genres      []string       `bson:"genres"` // optional
 	Description string         `bson:"description"`
 	WritingType string         `bson:"writing_type"`
 	CreatedAt   int64          `bson:"created_at,omitempty"`
