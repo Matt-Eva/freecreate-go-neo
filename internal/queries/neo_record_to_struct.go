44338b96a4e081c519cba4ce18f9b8c370c5cdb9
Matt Eva <matteweva@gmail.com>
2024-07-26 20:52:14 -0700

    finished fleshing out map to struct

--- internal/queries/neo_record_to_struct.go
+++ internal/queries/neo_record_to_struct.go
@@ -4,18 +4,16 @@ import (
 	"fmt"
 	"freecreate/internal/err"
 	"reflect"
-
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-func NeoRecordToStruct(record neo4j.Record, structPointer interface{})(err.Error){
+func NeoRecordToStruct(record map[string]any, structPointer interface{})(err.Error){
 	value := reflect.ValueOf(structPointer).Elem()
 	t := value.Type()
 
 	for i := 0; i < t.NumField(); i ++{
 		field := t.Field(i)
 		name := field.Name
-		recordValue, ok := record.Get(name)
+		recordValue, ok := record[name]
 		if !ok {
 			msg := fmt.Sprintf("neo4j record does not have attribute %s", name)
 			return err.New(msg)
@@ -30,23 +28,60 @@ func NeoRecordToStruct(record neo4j.Record, structPointer interface{})(err.Error
 				return sErr
 			}
 			fieldValue.SetString(valid)
-		case reflect.Int:
-		case reflect.Int8:
-		case reflect.Int16:
-		case reflect.Int32:
-		case reflect.Int64:
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			valid, iErr := convInt(recordValue, name)
+			if iErr.E != nil {
+				return iErr
+			}
+			fieldValue.SetInt(valid)
+		case reflect.Bool:
+			valid, iErr := convBool(recordValue, name)
+			if iErr.E != nil {
+				return iErr
+			}
+			fieldValue.SetBool(valid)
+		default:
+			return err.New("field value not of type int, bool, or string")
 		}
 	}
 
 	return err.Error{}
 }
 
-func convString(value any, name string)(string, err.Error){
-	valid, ok := value.(string)
-	if !ok {
-		msg := fmt.Sprintf("could not convert value of %s field to string", name)
+func convString(value interface{}, name string)(string, err.Error){
+	switch v := value.(type){
+	case string:
+		return v, err.Error{}
+	default:
+		msg := fmt.Sprintf("record field for '%s' is not a string", name)
 		return "", err.New(msg)
 	}
+}
 
-	return valid, err.Error{}
+func convInt(value interface{}, name string)(int64, err.Error){
+	switch v := value.(type){
+	case int64:
+		return v, err.Error{}
+	case int:
+		return int64(v), err.Error{}
+	case int8:
+		return int64(v), err.Error{}
+	case int16:
+		return int64(v), err.Error{}
+	case int32:
+		return int64(v), err.Error{}
+	default:
+		msg := fmt.Sprintf("map value for field %s is not an integer", name)
+		return 0, err.New(msg)
+	}
+}
+
+func convBool(value interface{}, name string)(bool, err.Error){
+	switch v := value.(type){
+	case bool:
+		return v, err.Error{}
+	default:
+		msg := fmt.Sprintf("record field for '%s' is not a boolean", name)
+		return false, err.New(msg)
+	}
 }
--- internal/queries/neo_record_to_struct_test.go
+++ internal/queries/neo_record_to_struct_test.go
@@ -1,1 +1,30 @@
 package queries
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+func TestNeoRecordToStruct(t *testing.T){
+	type TestStruct struct {
+		Field string
+		Number int
+		MyBool bool
+	}
+	testStruct := TestStruct{}
+	testMap := map[string]any {
+		"Field": "sup",
+		"Number": 600,
+		"MyBool": true,
+	}
+	cErr := NeoRecordToStruct(testMap, &testStruct)
+	if cErr.E != nil {
+		cErr.Log()
+		t.Fatalf("above error occurred")
+	}
+	if testStruct.Field != "sup" || testStruct.Number != 600 || testStruct.MyBool != true{
+		fmt.Println(testMap, testStruct)
+		t.Fatalf("could not convert test map to test struct")
+	}
+}
