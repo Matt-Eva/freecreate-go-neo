417b57ad0708701e87ce1fc207d8d9928d64df6e
Matt Eva <matteweva@gmail.com>
2024-07-29 11:29:19 -0700

    set up get master user from db function"

---
+++ internal/api/middleware/authenticate_user.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"freecreate/internal/err"
+	"net/http"
+
+	"github.com/rbcervilla/redisstore/v9"
+)
+
+func AuthenticateUser (r *http.Request, store *redisstore.RedisStore)(err.Error){
+	_, gErr := store.Get(r, "uid")
+	if gErr != nil {
+		return err.NewFromErr(gErr)
+	}
+
+	return err.Error{}
+}
--- internal/api/test_handlers/master_user.go
+++ internal/api/test_handlers/master_user.go
@@ -1,18 +1,55 @@
 package test_handlers
 
 import (
+	"context"
+	"freecreate/internal/api/middleware"
+	"freecreate/internal/err"
+	"freecreate/internal/queries"
 	"net/http"
+	"os"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/rbcervilla/redisstore/v9"
 )
 
-func HandleMasterUser( neo neo4j.DriverWithContext, store *redisstore.RedisStore)http.HandlerFunc{
+type returnUser struct {
+	Username string `json:"username"`
+	DisplayName string `json:"displayName"`
+}
+
+func HandleMasterUser( ctx context.Context, neo neo4j.DriverWithContext, store *redisstore.RedisStore)http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
-		handleMasterUser(w, r, neo, store)
+		handleMasterUser(w, r,ctx, neo, store)
 	}
 }
 
-func handleMasterUser(w http.ResponseWriter, r *http.Request, neo neo4j.DriverWithContext, store *redisstore.RedisStore){
-	
+func handleMasterUser(w http.ResponseWriter, r *http.Request,ctx context.Context, neo neo4j.DriverWithContext, store *redisstore.RedisStore){
+	aErr := middleware.AuthenticateUser(r, store)
+	if aErr.E != nil{
+		// user, uErr := getMasterUserFromDb(ctx, neo)
+	}
+}
+
+func getMasterUserFromDb(ctx context.Context, neo neo4j.DriverWithContext)(returnUser, err.Error){
+	dbName := os.Getenv("NEO_DB")
+	query := `
+		MATCH (u:User)
+		WHERE u.masterUser = true
+		RETURN u.username AS Username, u.displayName AS DisplayName
+	`
+	result, nErr := neo4j.ExecuteQuery(ctx, neo, query, nil, neo4j.EagerResultTransformer, neo4j.ExecuteQueryWithDatabase(dbName))
+	if nErr != nil {
+		return returnUser{}, err.NewFromErr(nErr)
+	}
+	if len(result.Records) < 1 {
+		return returnUser{}, err.New("no records returned from query")
+	}
+	resultMap := result.Records[0].AsMap()
+	user := returnUser{}
+	cErr := queries.NeoRecordToStruct(resultMap, &user)
+	if cErr.E != nil {
+return returnUser{}, err.Error{}
+	}
+
+	return user, err.Error{}
 }
