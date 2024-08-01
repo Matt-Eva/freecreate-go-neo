b625255c2ea61507d8e09fb11efde8b17bd98bf5
Matt Eva <matteweva@gmail.com>
2024-07-31 17:21:14 -0700

    updated content model to chapter model

---
+++ internal/models/chapter.go
@@ -0,0 +1,16 @@
+package models
+
+// chapters exist only in Mongo, not in Neo4j
+// shard key is creatorid
+// query for chapter is creatorid/neoid/uid
+
+type Chapter struct {
+	Uid string `bson:"uid,omitempty"`
+	CreatorId string `bson:"creator_id,omitempty`
+	NeoId     string         `bson:"neo_id,omitempty"`
+	Title string `bson:"title,omitempty"`
+	Published bool `bson:"published,omitempty"`
+	CreatedAt int64          `bson:"created_at,omitempty"`
+	UpdatedAt int64          `bson:"updated_at,omitempty`
+	Content   map[string]any `bson:"content"`
+}
